f64af7c4dd3c069838717c65765ecd1b7998e383
manavsharma111 <manavsharma3825@gmail.com>
2026-09-07 17:44:08 +0530

    fixed cors for razorpay callback

--- frontend/src/pages/Checkout/Livetracking.jsx
+++ frontend/src/pages/Checkout/Livetracking.jsx
@@ -122,7 +122,8 @@ const LiveTrackingMap = ({ deliveryBoy, user, deliveryCoords, userCoords }) => {
         attributionControl={false}
       >
         <TileLayer
-          url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png" // Premium Dark Mode Map Layer
+          url="https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Dark_Gray_Base/MapServer/tile/{z}/{y}/{x}"
+          attribution='Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ'
         />
 
         {/* Delivery Boy Marker */}
